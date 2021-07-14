6b8e1eb0acb50e4d83c4b9f26e4bfb391136c86e
Joe Sremba <jsremba@gmail.com>
2021-07-14 20:33:36 +0000

    adding vpc module. rewrote module

--- vpc.tf
+++ vpc.tf
@@ -1,62 +1,15 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
-
-    project_id   = var.project
-    network_name = "jsremba-network"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name           = "jsremba-subnet-01"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = "us-west1"
-        },
-        {
-            subnet_name           = "jsremba-subnet-02"
-            subnet_ip             = "10.100.20.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = "true"
-            subnet_flow_logs      = "true"
-            description           = "This subnet has a description"
-        },
-        {
-            subnet_name               = "jsremba-subnet-03"
-            subnet_ip                 = "10.100.30.0/24"
-            subnet_region             = "us-west1"
-            subnet_flow_logs          = "true"
-            subnet_flow_logs_interval = "INTERVAL_10_MIN"
-            subnet_flow_logs_sampling = 0.7
-            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-        }
-    ]
-
-    secondary_ranges = {
-        subnet-01 = [
-            {
-                range_name    = "subnet-01-secondary-01"
-                ip_cidr_range = "192.168.64.0/24"
-            },
-        ]
-
-        subnet-02 = []
-    }
-
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        },
-        {
-            name                   = "app-proxy"
-            description            = "route through proxy to reach app"
-            destination_range      = "10.50.10.0/24"
-            tags                   = "app-proxy"
-            next_hop_instance      = "app-proxy-instance"
-            next_hop_instance_zone = "us-west1-a"
-        },
-    ]
+module "network" {
+  source  = "app.terraform.io/jsremba-training/network/google"
+  version = "3.2.2"
+  # insert required variables here
+  project_id = var.project
+  network_name = "jsremba-network"
+ 
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
 }
